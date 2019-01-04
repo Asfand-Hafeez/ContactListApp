469d0aa9e9bf5178cc5587f5501ab610d6ae08f6
Asfand Hafeez <asfandhafeez@Asfands-MacBook-Pro.local>
2019-01-04 16:17:45 +0500

    Initial Commit

---
+++ ContactListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactListApp
+//
+//  Created by Asfand Hafeez on 04/01/2019.
+//  Copyright © 2019 Asfand Hafeez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
